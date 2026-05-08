15b91f81d6d49534f22a90add1a227491e36cf1b
Swaroop <akkanaboina@gmail.com>
2026-05-08 13:41:54 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,21 +1,22 @@
+require('dotenv').config();
+
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"admin",
-    database:"demo"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err)=>{
-
     if(err){
-        console.log("database connection error");
+        console.log("database connection error", err);
         return;
     }
 
     console.log("database connected");
-
 });
 
 module.exports = db;
