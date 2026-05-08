62170840a65ffaf727032398b2ccf3d6f69e278c
Swaroop <akkanaboina@gmail.com>
2026-05-08 13:19:37 +0530

    Update server to use dynamic port configuration

--- backend/server2.js
+++ backend/server2.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const PORT = process.env.PORT || 5000;
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
@@ -13,6 +13,8 @@ app.use(express.json());
 
 app.use("/", userRoutes);
 
-app.listen(5020, () => {
-    console.log("server running");
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
 });
