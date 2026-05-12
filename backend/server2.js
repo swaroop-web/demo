4c92edfc797eb6a65f5bc453c20a9e498ad23fb3
swaroop <akkanaboina@gmail.com>
2026-05-12 16:03:04 +0530

    updated

--- backend/server2.js
+++ backend/server2.js
@@ -1,20 +1,30 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
-const PORT = process.env.PORT || 5000;
+
 const userRoutes = require('./routes/userRoutes');
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 app.use(cors({
-    origin: ['https://resilient-brigadeiros-9f8e1b.netlify.app', 'http://localhost:5020'],
+    origin: [
+        'https://resilient-brigadeiros-9f8e1b.netlify.app',
+        'http://localhost:5020'
+    ],
     credentials: true
 }));
+
 app.use(express.json());
 
 app.use("/", userRoutes);
 
-
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
